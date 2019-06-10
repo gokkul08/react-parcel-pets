131506cfb05f4397ba65eb032a9ec98081d9b17a
gokkul08 <agn253@nyu.edu>
2019-06-10 00:31:36 -0400

    Adding Hooks to React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1 id="main">Adopt Me!</h1>
-      <Pet name="Chilli" animal="Dog" breed="Labrador" />
-      <Pet name="Pepper" animal="Bird" breed="Parrot" />
-      <Pet name="Ginger" animal="Cat" breed="Silk" />
+      <SearchParams />
     </div>
   );
 };
